c3564c9b2aabe204a26be845b384b450f46a9863
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-08-15 14:47:40 -0300

    Editando barra de navegação, templates e css

--- empresa/views.py
+++ empresa/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def cadastrar_empresa(request):
-    return HttpResponse('Cadastrar Empresa')
+    if request.method == "GET":
+        return render(request, 'cadastrar_empresa.html')
