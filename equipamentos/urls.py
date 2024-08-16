cc91b36e8ee10cdb3cb107bac1377991a2c85191
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-08-16 13:21:03 -0300

    Cadastro de Marca e Cadastro de Software

--- equipamentos/urls.py
+++ equipamentos/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastrar_equipamento/', views.cadastrar_equipamento, name="cadastrar_equipamento"),
+    # marca
+    path('cadastrar_marca/', views.cadastrar_marca, name="cadastrar_marca"),
+
+    # software
+    path('cadastrar_software/', views.cadastrar_software, name="cadastrar_software")
 ]
--- equipamentos/views.py
+++ equipamentos/views.py
@@ -1,6 +1,45 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Computador, Equipamento,LicencaSoftware, Marca
+from .models import SistemaOperacional,Software,TipoEquipamento, Setor
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
-def cadastrar_equipamento(request):
-    return HttpResponse('Cadastrar Equipamento')
+def cadastrar_marca(request):
+    if request.method == "GET":
+        return render(request,'cadastrar_marca.html')
+    
+    elif request.method == "POST":
+        nome_marca = request.POST.get('nome_marca')
+
+        try:
+            marca = Marca(nome_marca=nome_marca)
+
+            marca.save()
+
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/equipamentos/cadastrar_marca') 
+        
+        messages.add_message(request, constants.SUCCESS, 'Marca criada com sucesso')
+        return redirect('/equipamentos/cadastrar_marca')
+
+def cadastrar_software(request):
+    if request.method == "GET":
+        return render(request,'cadastrar_software.html')
+    
+    elif request.method == "POST":
+        nome_software = request.POST.get('nome_software')
+
+        try:
+            software = Software(nome_software=nome_software)
+
+            software.save()
+
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/equipamentos/cadastrar_software') 
+        
+        messages.add_message(request, constants.SUCCESS, 'Software criado com sucesso')
+        return redirect('/equipamentos/cadastrar_software')
