b0dd22ca18abd9fb005e0ef48b44f02389afcfe1
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-08-28 11:21:00 -0300

    Criando model de Ocorrencia de Usuários

--- ocorrencias/models.py
+++ ocorrencias/models.py
@@ -4,11 +4,13 @@ from usuarios.models import Usuario, Setor
 # Create your models here.
 class OcorrenciaUsuario(models.Model):
     tipo_ocorrencia_choices = (
-        ('INATIVAR USUÁRIO', '1'),               
-        ('TROCAR USUÁRIO DE SETOR', '2'),
+        ('1', 'INATIVAR USUÁRIO'),               
+        ('2', 'TROCAR USUÁRIO DE SETOR'),
     )
-
-
+    tipo_ocorrencia = models.CharField(max_length=1, choices=tipo_ocorrencia_choices)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING, blank=True)
+    setor = models.ForeignKey(Setor, on_delete=models.DO_NOTHING, blank=True)
+    observacao = models.TextField(blank=True)
 """
 class OcorrenciaComputador(models.Model):
     pass
