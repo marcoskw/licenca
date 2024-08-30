4974e0f443e82e0d43b9c404247642f448690982
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-08-30 16:09:03 -0300

    Criado view atualizar_setor_usuario

--- ocorrencias/urls.py
+++ ocorrencias/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     # Usuários
-    path('inativar_usuario/', views.inativar_usuario, name="inativar_usuario"),
+    path('ocorrencias_usuarios/', views.ocorrencias_usuarios, name="ocorrencias_usuarios"),
+    path('ocorrencias_usuarios/inativar_usuario/', views.inativar_usuario, name="inativar_usuario"),
+    path('ocorrencias_usuarios/atualizar_setor_usuario/', views.atualizar_setor_usuario, name='atualizar_setor_usuario'),
+    
+    # Equipamentos
+    path('ocorrencias_equipamentos/', views.ocorrencias_equipamentos, name="ocorrencias_equipamentos"),
+
 ]
--- ocorrencias/views.py
+++ ocorrencias/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import redirect, render
 from ocorrencias.models import OcorrenciaUsuario
-from usuarios.models import Usuario
+from usuarios.models import Usuario, Setor
 
 from django.contrib import messages
 from django.contrib.messages import constants
 
-# Create your views here.
+# Usuários
+def ocorrencias_usuarios(request): 
+    return render(request, 'ocorrencias_usuarios.html')
+
 def inativar_usuario(request):
     usuarios = Usuario.objects.all()
 
@@ -37,3 +40,44 @@ def inativar_usuario(request):
         
         messages.add_message(request, constants.SUCCESS, 'Ocorrencia criada com sucesso')
         return redirect('/ocorrencias/inativar_usuario')   
+
+def atualizar_setor_usuario(request):
+    usuarios = Usuario.objects.all()
+    setores = Setor.objects.all()
+
+    if request.method == "GET":
+        return render(request,'atualizar_setor_usuario.html', {
+            'usuarios': usuarios,
+            'setores': setores})
+
+    elif request.method == "POST":
+        usuario_selecionado = request.POST.get('usuario')
+        observacoes = request.POST.get('observacoes')
+        setor_id= request.POST.get('setor_novo')
+        tipo_ocorrencia = 2
+        usuario = Usuario.objects.get(id=usuario_selecionado)
+        setor = Setor.objects.get(id=setor_id)
+
+        ocorrencia = OcorrenciaUsuario(
+            tipo_ocorrencia=tipo_ocorrencia,
+            usuario=usuario,
+            setor=setor,
+            observacoes=observacoes,
+        )
+
+        try:
+            usuario.setor = setor
+            ocorrencia.save()
+            usuario.save()
+
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('atualizar_setor_usuario')   
+        
+        messages.add_message(request, constants.SUCCESS, 'Ocorrencia criada com sucesso')
+        return redirect('atualizar_setor_usuario')   
+
+
+# Equipamentos
+def ocorrencias_equipamentos(request):
+    return render(request, 'ocorrencias_equipamentos.html')
