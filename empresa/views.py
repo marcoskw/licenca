3508a1850a1405e7690a9b92300a0d42af28f787
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-10-21 10:56:32 -0300

    Adicionando listagem de operadores somente Ativos!

--- empresa/views.py
+++ empresa/views.py
@@ -164,7 +164,7 @@ def listar_operadores(request):
         return redirect('/login')
         
     if request.method == "GET":
-        operadores = Operador.objects.order_by('nome_operador')
+        operadores = Operador.objects.filter(status='ATV').order_by('nome_operador')
         
     return render(request, 'listar_operadores.html', {'operadores': operadores})
 
